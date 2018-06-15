463d75157dca0c84670e31053483825d2aeebb6b
Sean McPherson <seanmcp25@gmail.com>
2018-06-15 06:28:29 -0400

    Add endpoint to get specific student
    Look into how to throw an error when student is not found.

--- api/index.py
+++ api/index.py
@@ -8,15 +8,28 @@ app = Flask(__name__)
 def welcome():
     return 'Welcome to the Popsicle Sticks API, powered by Python!'
 
+# Section routes
+
 @app.route('/sections')
-def get_sections():
+def get_list_sections():
     res = get_data('sections')
     return jsonify(res)
 
+# Student routes
+
 @app.route('/students')
-def get_students():
+def get_list_students():
     res = get_data('students')
     return jsonify(res)
+
+@app.route('/student/<id>')
+def get_student_by_id(id):
+    data = get_data('students')
+    for student in data:
+        if student['id'] == id:
+            return jsonify(student)
+        else:
+            return 'No student found'
 
 # Utility functions
 def read_json():
