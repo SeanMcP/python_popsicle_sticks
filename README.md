30ba400744e94e1dd39bbd1ad90c5de3d139e485
Sean McPherson <seanmcp25@gmail.com>
2018-06-15 06:19:26 -0400

    Fix response error with jsonify
    I needed to import jsonify from flask to wrap my responses.

--- README.md
+++ README.md
@@ -2,4 +2,4 @@
 
 This is a Python/Flask API for the [Popsicle Sticks app](https://github.com/SeanMcP/popsicle-sticks).
 
-Heavily modified from [Auth0's](https://auth0.com) ["Developing RESTful APIs with Python and Flask"](https://auth0.com/blog/developing-restful-apis-with-python-and-flask/) tutorial.
+This API takes some inspiration from [Auth0's](https://auth0.com) ["Developing RESTful APIs with Python and Flask"](https://auth0.com/blog/developing-restful-apis-with-python-and-flask/) tutorial.
--- api/index.py
+++ api/index.py
@@ -1,22 +1,27 @@
-from flask import Flask
+from flask import Flask, jsonify
 # from utils import shuffle
 import json
 
 app = Flask(__name__)
 
 @app.route('/')
-def hello_world():
-    return 'Hello, World!'
+def welcome():
+    return 'Welcome to the Popsicle Sticks API, powered by Python!'
+
+@app.route('/sections')
+def get_sections():
+    res = get_data('sections')
+    return jsonify(res)
 
 @app.route('/students')
 def get_students():
-    return 'Students'
+    res = get_data('students')
+    return jsonify(res)
 
 # Utility functions
 def read_json():
     with open('./api/data.json') as raw:
-        data = json.load(raw)
-    return data 
+        return json.load(raw)
 
 def get_data(key):
     return read_json()[key]
