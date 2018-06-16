b518022e9418d8e094c201c0b24435ac9123995d
Sean McPherson <seanmcp25@gmail.com>
2018-06-15 22:57:13 -0400

    Add endpoint to add student
    Moved data files to separate data directory. Cleaned up old endpoints.

--- api/index.py
+++ api/index.py
@@ -25,7 +25,7 @@ def post_add_section():
             'title': request.form['title']
         }
         sections = read_file('sections')
-        with open('./api/sections.json', 'w') as file:
+        with open('./data/sections.json', 'w') as file:
             sections.append(section)
             json.dump(sections, file)
         return 'Success'
@@ -34,20 +34,36 @@ def post_add_section():
 
 @app.route('/students')
 def get_list_students():
-    res = get_data('students')
+    res = read_file('students')
     return jsonify(res)
+
 
 @app.route('/students/name')
 def get_list_student_names():
-    students = get_data('students')
+    students = read_file('students')
     res = []
     for student in students:
         res.append(student['name'])
     return jsonify(res)
 
+@app.route('/student', methods=['POST'])
+def post_add_student():
+    if request.method == 'POST':
+        student = {
+            'gender': request.form['gender'],
+            'id': gen_id(),
+            'name': request.form['name'],
+            'section_id': request.form['section_id']
+        }
+        students = read_file('students')
+        with open('./data/students.json', 'w') as file:
+            students.append(student)
+            json.dump(students, file)
+        return 'Success'
+
 @app.route('/student/<id>')
 def get_student_by_id(id):
-    students = get_data('students')
+    students = read_file('students')
     for student in students:
         if student['id'] == id:
             return jsonify(student)
@@ -55,16 +71,9 @@ def get_student_by_id(id):
             return 'None found'
 
 # Utility functions
-def read_json():
-    with open('./api/data.json') as raw:
-        return json.load(raw)
-
 def read_file(file_name):
-    with open(f'./api/{file_name}.json') as raw:
+    with open(f'./data/{file_name}.json') as raw:
         return json.load(raw)
 
 def gen_id():
     return str(uuid.uuid4())
-
-def get_data(key):
-    return read_json()[key]
